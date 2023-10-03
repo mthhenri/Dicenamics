fc978459d6f9b6d3f4138af49c7ba618c7cbbe71
Ygor Espada <ygorespada14@gmail.com>
2023-10-03 16:01:43 -0300

    Edição de Usuario

--- Dicenamics/Dicenamics/Models/Usuario.cs
+++ Dicenamics/Dicenamics/Models/Usuario.cs
@@ -1,5 +1,20 @@
 namespace Dicenamics.Models;
-public class Usuario
-{
-    public int UsuarioId { get; set; }
-}
+
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string? Username { get; set; }
+        public string? Nickname { get; set; }
+        public string? Senha { get; set; }
+        public List<DadoSimples> DadosPessoais { get; set; }
+
+        public bool CompararUsername(string outroUsername)
+        {
+            return string.Equals(Username, outroUsername, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public bool VerificarSenha(string senhaDigitada)
+        {
+            return string.Equals(Senha, senhaDigitada);
+        }
+    }
