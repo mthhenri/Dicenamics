73b6d800bea30ebf53645f521a45900ce9dc8b26
Ygor Espada <ygorespada14@gmail.com>
2023-10-04 16:14:38 -0300

    Controller Usuario edição

--- Dicenamics/Dicenamics/Controllers/UsuarioController.cs
+++ Dicenamics/Dicenamics/Controllers/UsuarioController.cs
@@ -1,88 +1,85 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Dicenamics.Models;
 using Dicenamics.Data;
 
-namespace Dicenamics.Controllers;
 
-[ApiController]
-[Route("dicenamics/usuario")]
-public class UsuarioController : ControllerBase
+namespace Dicenamics.Controllers
 {
-    private readonly AppDatabase _ctx;
-
-    public UsuarioController(AppDatabase context)
+    [Route("Controllers/UsuarioController")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
     {
-        _ctx = context;
-    }
+        private readonly AppDatabase _ctx;
 
-    //Colocar metodo http com rota
-    public async Task<IActionResult> ListarUsuarios()
-    {
-        //Try catch
-        var usuarios = await _ctx.Usuarios.ToListAsync();
-        return View(usuarios);
-    }
-
-    //Colocar metodo http com rota
-    public IActionResult Criar()
-    {
-        //??
-        return View();
-    }
-
-    [HttpPost] //Rota
-    public async Task<IActionResult> Criar(Usuario usuario)
-    {
-        //try catch
-        //??
-        if (ModelState.IsValid)
+        public UsuarioController(AppDatabase context)
         {
-            _ctx.Add(usuario);
+            _ctx = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CriarUsuario(Usuario usuario)
+        {
+            if (usuario == null)
+            {
+                return BadRequest();
+            }
+
+            _ctx.Usuarios.Add(usuario);
             await _ctx.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
-        return View(usuario);
-    }
 
-    //Colocar metodo http com rota
-    public async Task<IActionResult> Editar(int? id)
-    {
-        //try catch
-        //??
-        if (id == null)
-        {
-            return NotFound();
+            return CreatedAtRoute("GetUsuario", new { id = usuario.Id }, usuario);
         }
 
-        var usuario = await _ctx.Usuarios.FindAsync(id);
-        if (usuario == null)
+        [HttpGet]
+        public async Task<IActionResult> BuscarTodosUsuarios()
         {
-            return NotFound();
+            var usuarios = await _ctx.Usuarios.ToListAsync();
+            return Ok(usuarios);
         }
 
-        return View(usuario);
-    }
-
-    [HttpPost] //Rota
-    public async Task<IActionResult> Editar(int id, Usuario usuario)
-    {
-        //Try catch
-        if (id != usuario.UsuarioId)
+        [HttpGet("{id}", Name = "GetUsuario")]
+        public async Task<IActionResult> BuscarUsuarioPorId(int id)
         {
-            return NotFound();
+            var usuario = await _ctx.Usuarios.FindAsync(id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(usuario);
         }
 
-        if (ModelState.IsValid)
+        [HttpGet("Usuario/{username}")]
+        public async Task<IActionResult> BuscarUsuarioPorUsername(string username)
         {
+            var usuario = await _ctx.Usuarios.FirstOrDefaultAsync(u => u.Username == username);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(usuario);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> AtualizarUsuario(int id, Usuario usuarioAtualizado)
+{
+            if (id != (int)usuarioAtualizado.Id)
+            {
+                return BadRequest();
+            }  
+
+            _ctx.Entry(usuarioAtualizado).State = EntityState.Modified;
+
             try
             {
-                _ctx.Update(usuario);
                 await _ctx.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!UsuarioExiste(usuario.UsuarioId))
+                if (!UsuarioExists(id))
                 {
                     return NotFound();
                 }
@@ -91,85 +88,29 @@ public class UsuarioController : ControllerBase
                     throw;
                 }
             }
-            return RedirectToAction("Index");
+
+            return NoContent();
         }
-        return View(usuario);
-    }
 
-    // Metodo criado para para buscar username de usuario e caso ja existe ele retornar 
-    //Colocar metodo http com rota
-    private bool UsuarioExiste(int usuarioId)
-    {
-        //??
-        throw new NotImplementedException();
-    }
-
-    //Colocar metodo http com rota
-    public async Task<IActionResult> Excluir(int? id)
-    {
-        //try catch
-        //??
-        if (id == null)
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> ExcluirUsuario(int id)
         {
-            return NotFound();
-        }
+            var usuario = await _ctx.Usuarios.FindAsync(id);
 
-        var usuario = await _ctx.Usuarios.FirstOrDefaultAsync(m => m.UsuarioId == id);
-        if (usuario == null)
-        {
-            return NotFound();
-        }
-
-        return View(usuario);
-    }
-
-    [HttpPost, ActionName("Excluir")] //??
-    public async Task<IActionResult> ExcluirConfirmado(int id)
-    {
-        //try catch
-        //??
-        var usuario = await _ctx.Usuarios.FindAsync(id);
-        _ctx.Usuarios.Remove(usuario);
-        await _ctx.SaveChangesAsync();
-        return RedirectToAction("Index");
-    }
-
-    // Colocar metodo http e rota
-    public async Task<IActionResult> BuscarPorId(int? id)
-    {
-        //try catch
-        //??
-        if (id == null)
-        {
-            return NotFound();
-        }
-
-            var usuario = await _ctx.Usuarios.FirstOrDefaultAsync(m => m.UsuarioId == id);
             if (usuario == null)
-        {
-            return NotFound();
+            {
+                return NotFound();
+            }
+
+            _ctx.Usuarios.Remove(usuario);
+            await _ctx.SaveChangesAsync();
+
+            return NoContent();
         }
 
-            return View(usuario);
-    }
-
-    // Colocar metodo http e rota
-    public async Task<IActionResult> BuscarPorUsername(string username)
-    {
-        //try catch
-        //??
-        if (string.IsNullOrEmpty(username))
+        private bool UsuarioExists(int id)
         {
-            return NotFound();
+            return _ctx.Usuarios.Any(u => (int)u.Id == id);
         }
-
-            var usuario = await _ctx.Usuarios.FirstOrDefaultAsync(m => m.Username == username);
-            if (usuario == null)
-        {
-            return NotFound();
-        }
-
-            return View(usuario);
     }
 }
-
