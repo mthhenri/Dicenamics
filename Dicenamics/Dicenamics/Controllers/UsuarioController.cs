e6688c728b9cbcca0e407ee321e439772b0dd5fe
Ygor Espada <ygorespada14@gmail.com>
2023-10-04 15:17:48 -0300

    CRUDs UsuarioController

--- Dicenamics/Dicenamics/Controllers/UsuarioController.cs
+++ Dicenamics/Dicenamics/Controllers/UsuarioController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Dicenamics.Models;
 using Dicenamics.Data;
+
 namespace Dicenamics.Controllers
 {
     public class UsuarioController : Controller
@@ -12,12 +14,137 @@ namespace Dicenamics.Controllers
             _ctx = context;
         }
 
-        [HttpPost]
-        public IActionResult VerificarUsuarioExistente(string username)
+        public async Task<IActionResult> Index()
         {
-            bool usuarioExiste = _ctx.Usuarios.Any(u => u.Username == username);
+            var usuarios = await _ctx.Usuarios.ToListAsync();
+            return View(usuarios);
+        }
 
-            return Ok(usuarioExiste);
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Criar(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _ctx.Add(usuario);
+                await _ctx.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(usuario);
+        }
+
+        public async Task<IActionResult> Editar(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _ctx.Usuarios.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Editar(int id, Usuario usuario)
+        {
+            if (id != usuario.UsuarioId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _ctx.Update(usuario);
+                    await _ctx.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UsuarioExiste(usuario.UsuarioId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(usuario);
+        }
+
+        // Metodo criado para para buscar username de usuario e caso ja existe ele retornar 
+        private bool UsuarioExiste(int usuarioId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IActionResult> Excluir(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _ctx.Usuarios.FirstOrDefaultAsync(m => m.UsuarioId == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        [HttpPost, ActionName("Excluir")]
+        public async Task<IActionResult> ExcluirConfirmado(int id)
+        {
+            var usuario = await _ctx.Usuarios.FindAsync(id);
+            _ctx.Usuarios.Remove(usuario);
+            await _ctx.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> BuscarPorId(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+                var usuario = await _ctx.Usuarios.FirstOrDefaultAsync(m => m.UsuarioId == id);
+                if (usuario == null)
+            {
+                return NotFound();
+            }
+
+                return View(usuario);
+        }
+
+        public async Task<IActionResult> BuscarPorUsername(string username)
+        {
+            if (string.IsNullOrEmpty(username))
+            {
+                return NotFound();
+            }
+
+                var usuario = await _ctx.Usuarios.FirstOrDefaultAsync(m => m.Username == username);
+                if (usuario == null)
+            {
+                return NotFound();
+            }
+
+                return View(usuario);
         }
     }
 }
