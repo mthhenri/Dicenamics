14b50c98bdc56060fa78f921e6b94aaeb036c143
mthhenri <mathsilvaesilva@gmail.com>
2023-11-11 13:12:15 -0300

    Inicio do front end com esqueleto da tela incial

--- Dicenamics/FrontEnd/src/app/app-routing.module.ts
+++ Dicenamics/FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // inserir as rotas aqui
+  {
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Dicenamics/FrontEnd/src/app/app.module.ts
+++ Dicenamics/FrontEnd/src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
